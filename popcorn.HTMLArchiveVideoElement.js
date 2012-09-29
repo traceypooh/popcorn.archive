70ac74e42a2e51e43680afd7bb31f8f44f3b56fb
Tracey Jaquith <tracey@vm-home1.us.archive.org>
2012-09-29 22:26:52 +0000

    major reorg for sanity.  now should be able to have 2+ IA players, properly each confined

--- popcorn.HTMLArchiveVideoElement.js
+++ popcorn.HTMLArchiveVideoElement.js
@@ -6,8 +6,10 @@
 (function( Popcorn, document ) {
 
   var debug=true;
-  var playerReady = false;
+  var flash=false;
 
+  window.iaplayer={};
+    
   var log = function(obj){
     if (debug  &&  typeof console !='undefined'){
       console.log('================================');
@@ -23,49 +25,60 @@
     return "maybe";
   }
 
-  window.flashReady = function() {
-    log('flashReady!');
-    playerReady = true;
-  };
-   
   
 
-  var archiveInit = function( itemMetadata ){
+   
+  function wrapMedia( id, mediaType ) {
 
-    var id='video';//xxx
-    var width=640;//xxxx
-    var height=480 + (debug ? 30 : 0);//xxxx
-    var identifier=itemMetadata.metadata.identifier;
+    window.iaplayer[id] = {
+      iaid:'commute',//xxx media.src.match(/([^/]+)$/)[0];
+      id:id,
+      playerReady:false,
+      flash:false,
+      autoPlay:false,
+      
+      setup:function(){
+        // this allows us to find the best video(/audio) file to play!
+        var me=window.iaplayer[id];
+        Popcorn.getJSONP("http://archive.org/metadata/"+me.iaid+"?&callback=jsonp", 
+                        me.init );
+      },
 
-    var bestfi=false;
-    var audio=false;
-    log(itemMetadata.files);
-    // find best flash playable IA "derivative" for video
-    for (i=0; i<itemMetadata.files.length; i++)
-    {
-      var fi=itemMetadata.files[i];
-      if (fi.format=='h.264')       { bestfi = fi; break; }
-      if (fi.format=='512Kb MPEG4') { bestfi = fi; break; }
-    }
-    if (bestfi===false){
-      // find best flash playable IA "derivative" for audio
-      for (i=0; i<itemMetadata.files.length; i++)
-      {
-        var fi=itemMetadata.files[i];
-        if (fi.format=='VBR MP3')     { bestfi = fi; break; }
-        if (fi.format=='MP3')         { bestfi = fi; break; }
-      }
-      audio=true;
-    }
-    log('bestfi: '+bestfi.name);
+      
+      init:function( itemMetadata ){
+        var width=(debug ? 320 : 640);//xxxx
+        var height=(debug ? 240 : 480) + (debug ? 30 : 0);//xxxx
+        
+        var bestfi=false;
+        var audio=false;
+        log(itemMetadata.files);
+        // find best flash playable IA "derivative" for video
+        for (i=0; i<itemMetadata.files.length; i++)
+        {
+          var fi=itemMetadata.files[i];
+          if (fi.format=='h.264')       { bestfi = fi; break; }
+          if (fi.format=='512Kb MPEG4') { bestfi = fi; break; }
+        }
+        if (bestfi===false){
+          // find best flash playable IA "derivative" for audio
+          for (i=0; i<itemMetadata.files.length; i++)
+          {
+            var fi=itemMetadata.files[i];
+            if (fi.format=='VBR MP3')     { bestfi = fi; break; }
+            if (fi.format=='MP3')         { bestfi = fi; break; }
+          }
+          audio=true;
+        }
+        log('bestfi: '+bestfi.name);
 
     
-    var flashvars = {
+        var me=window.iaplayer[id];
+        var flashvars = {
           "netstreambasepath":"http%3A%2F%2Farchive.org%2F",
           "controlbar.position":(audio ? "top" : "over"),
-          "playerready":"window.flashReady",
+          "playerready":"window.iaplayer."+id+".flashReady",
           "id":id,
-          "file":"%2Fdownload%2F"+identifier + encodeURIComponent('/'+bestfi.name)
+          "file":"%2Fdownload%2F"+me.iaid + encodeURIComponent('/'+bestfi.name)
         };
         if (audio) {
           flashvars.provider="sound";
@@ -78,56 +91,68 @@
           if (debug)
             flashvars["controlbar.position"]="bottom";
         }
-        
 
         attributes = {
-          "name":identifier
+          "name":me.iaid
         };
 
-
-    //  extend options from user to flashvars. NOTE: Videos owned by Plus Archive users may override these options
-    var options=[];
+        //  extend options from user to flashvars.
+        var options=[];
     
-    Popcorn.extend( flashvars, options );
+        Popcorn.extend( flashvars, options );
 
-    params = {allowscriptaccess: "always",
-              allowfullscreen: "true",
-                enablejs: "true",
-                seamlesstabbing: "true",
-                wmode: "transparent"
-             };
-    
+        params = {allowscriptaccess: "always",
+                  allowfullscreen: "true",
+                  enablejs: "true",
+                  seamlesstabbing: "true",
+                  wmode: "transparent"
+                 };
 
-    swfobject.embedSWF( "http://archive.org/jw/player.swf", id,
-                        width, (audio ? 60 : height), "9.0.0", "expressInstall.swf",
-                        flashvars, params, attributes );
-    
+        swfobject.embedSWF("http://archive.org/jw/player.swf", id,
+                           width, (audio ? 60 : height), "9.0.0", "expressInstall.swf",
+                           flashvars, params, attributes );
+        log(me.playerReady);
+      },
 
-    log(playerReady);
-  }
-  ;
-  //end var archiveInit = function()...
-
-   
-   
-  function wrapMedia( id, mediaType ) {
-    
-    var parent = typeof id === "string" ? document.querySelector( id ) : id;
-
-   
-    var archiveSetup = function(){
-      var identifier=['commute'];//xxx media.src.match(/([^/]+)$/);
       
-      identifier=identifier[0];
-      
-      // this allows us to find the best video(/audio) file to play!
-      Popcorn.getJSONP("http://archive.org/metadata/"+identifier+"?&callback=jsonp", archiveInit );
+      stateChanged:function( val ){
+        log('statechanged '+val);//xxx
+      },
+      timed:function( val ){ //xxx
+      },
+      buffered:function( val ){ //xxx
+      },
+      loaded:function( val ){ //xxx
+      },
+      volumed:function( val ){ //xxx
+      },
+      muted:function( val ){ //xxx
+      },
+      errored:function( val ){ //xxx
+      },
+      flashReady:function() {
+        log('flashReady!');
+        this.playerReady = true;
+        this.flash = $('#'+this.id).get(0);//xxx jQuery!
+
+        this.flash.addModelListener     ("STATE", "window.iaplayer."+this.id+".stateChanged");
+        this.flash.addModelListener     ("TIME" , "window.iaplayer."+this.id+".timed");
+        this.flash.addModelListener     ("BUFFER","window.iaplayer."+this.id+".buffered");
+        this.flash.addModelListener     ("LOADED","window.iaplayer."+this.id+".loaded");
+        this.flash.addControllerListener("VOLUME","window.iaplayer."+this.id+".volumed");
+        this.flash.addControllerListener("MUTE",  "window.iaplayer."+this.id+".muted");
+        this.flash.addModelListener     ("ERROR", "window.iaplayer."+this.id+".errored");
+        
+        if (this.autoPlay)
+          this.flash.sendEvent('PLAY');
+      }
     };
+      
     
     if ( !window.swfobject )
-      Popcorn.getScript( "http://archive.org/jw/popcorn/swfobject.js", archiveSetup );
+      Popcorn.getScript("http://archive.org/jw/popcorn/swfobject.js", window.iaplayer[id].setup);
     else
-      archiveSetup();
+      window.iaplayer[id].setup();
   
 
     // Add the helper function _canPlaySrc so this works like other wrappers.
@@ -137,33 +162,34 @@
     //parent.appendChild( media );
     //media._canPlaySrc = canPlaySrc;
 
-    //var flash = document.getElementById( 'video' );//xxxx
-    window.flash = function() { return $('#video').get(0);/*xxxxx*/ };
 
+    var player=window.iaplayer[id];
 
     media.play = function(){
       log('mplay');
-      flash().sendEvent('PLAY');
+      player.flash.sendEvent('PLAY');
     };
     media.pause = function(){
       log('mpause');
-      flash().sendEvent('PLAY', false);
+      player.flash.sendEvent('PLAY', false);
     };
 
-
+    
+    var impl = window.impl;//xxxxx
+    
     var isMuted = function() {
-      if (!playerReady) 
+      if (!player.playerReady) 
         return false;
-      return (flash().getConfig().mute ? true : false);
+      return (player.flash.getConfig().mute ? true : false);
     };
 
     Object.defineProperties( media, {
       src: {
         get: function() {
-          return window.impl.src;
+          return impl.src;
         },
         set: function( aSrc ) {
-          if( aSrc && aSrc !== window.impl.src ) {
+          if( aSrc && aSrc !== impl.src ) {
             log(aSrc);
             //changeSrc( aSrc );//xxxxxxx
           }
@@ -173,36 +199,59 @@
         get: function() {
           return isMuted();
         },
-        set: function( aSrc ) {
-          log(aSrc);
-          if ( isMuted() !== aSrc ) 
-            flash().sendEvent('MUTE');
+        set: function( val ) {
+          if (!player.playerReady)
+            return;
+          
+          if ( isMuted() !== val ) 
+            player.flash.sendEvent('MUTE');
         }
       },
       volume:{
         set: function( val ) {
-          if (!playerReady)
+          if (!player.playerReady)
             return 1;
           
-          var volNow = flash().getConfig().volume / 100;
+          var volNow = player.flash.getConfig().volume / 100;
               
           if ( !val || typeof val !== "number" || ( val < 0 || val > 1 ) ) {
             return volNow;
           }
 
           if ( volNow !== val ) {
-            flash().sendEvent("VOLUME", val * 100 );
-            media.dispatchEvent( "volumechange" );
-            log('volly');
+            player.flash.sendEvent("VOLUME", val * 100 );
+            //media.dispatchEvent( "volumechange" );//xxxx
           }
 
           return volNow;
         },
         get: function() {
-          if (!playerReady)
+          if (!player.playerReady)
             return 1;
           
-          return flash().getConfig().volume / 100;
+          return player.flash.getConfig().volume / 100;
+        }
+      },
+      currentTime:{
+        set:function( val ){
+          if (!player.playerReady)
+            return impl.currentTime;
+          
+          if ( !val )
+            return impl.currentTime;
+
+          impl.currentTime = +val;
+          seeking = true;
+
+          //media.dispatchEvent( "seeked" );//xxxx
+          //media.dispatchEvent( "timeupdate" );//xxxx
+              
+          player.flash.sendEvent("SEEK", impl.currentTime ); // (float #seconds)
+          
+          return impl.currentTime;
+        },
+        get:function( ){
+          return impl.currentTime;
         }
       }
     });
@@ -218,6 +267,9 @@
 
 
   function HTMLArchiveVideoElement( id ) {
+    log(id);
+    var parent = typeof id === "string" ? document.getElementById( id ) : id;//xxx
+   
     var self = this;
     var EMPTY_STRING = "",
       MIN_WIDTH = 320,
@@ -234,8 +286,6 @@
         loop: false,
         poster: EMPTY_STRING,
         volume: 1,
-        // Vimeo has no concept of muted, store volume values
-        // such that muted===0 is unmuted, and muted>0 is muted.
         muted: 0,
         currentTime: 0,
         duration: NaN,
